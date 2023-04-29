a6a65bdf9fccd4740825fad50b1e2db820a41796
Slummen <127329687+Slummen@users.noreply.github.com>
2023-04-29 14:29:11 +0200

    Delete PRO1000-hoved directory

--- PRO1000-hoved/Forside/hover.js
+++
@@ -1,9 +0,0 @@
-const image = document.querySelector('.guliphone');
-image.addEventListener('mouseover', () => {
-  image.style.transform = 'scale(1.2)';
-  image.style.transition = 'transform 0.5s ease';
-});
-image.addEventListener('mouseout', () => {
-  image.style.transform = 'scale(1)';
-  image.style.transition = 'transform 0.5s ease';
-});
